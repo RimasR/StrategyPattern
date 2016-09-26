1a4a8adeabd64caac60795136248b2de9dd8520a
Rimantas Radžiūnas <rimantas.radziunas@mif.stud.vu.lt>
2016-09-26 22:40:44 +0300

    Add project files.

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Car car = new Car();
+            Airplane airplane = new Airplane();
+
+            switch (Int32.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    car.MoveBehavior = new Drive();
+                    car.Move();
+                    break;
+
+                case 2:
+                    airplane.MoveBehavior = new Fly();
+                    airplane.Move();
+                    break;
+            }
+        }
+    }
+}
---
+++ StrategyPattern/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class Vehicle
+    {
+        public IMoveBehavior MoveBehavior;
+
+        public Vehicle()
+        {
+            
+        }
+
+        public void Move()
+        {
+            MoveBehavior.Move(this);
+        }
+    }
+
+}
